7cc62dc0ceb72670a2b98c460b58371bc7c01a0d
Juan Quintero <juan_seb.quintero@uao.edu.co>
2017-06-11 20:08:45 -0500

    Fixed HTTPS

--- src/app/giphy.service.ts
+++ src/app/giphy.service.ts
@@ -5,27 +5,26 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class GiphyService {
-  private giphyAPIBase = 'http://api.giphy.com/v1/gifs';
+  private giphyAPIBase = 'https://api.giphy.com/v1/gifs';
   private APIKEY = 'dc6zaTOxFJmzC';
 
   constructor(private http: Http) { }
 
   getRandomGif(): Observable<Response> {
-    return this.http.get(this.giphyAPIBase + '/random?api_key=' 
-      + this.APIKEY + '/')
+    return this.http.get(this.giphyAPIBase + '/random?api_key=' + this.APIKEY)
       .map((res) => res.json());
   }
 
   getTrendingGifs(offset, limit): Observable<Response> {
     return this.http.get(this.giphyAPIBase + '/trending?api_key='
-      + this.APIKEY + '&offset=' + offset + '&limit=' + limit + '/')
+      + this.APIKEY + '&offset=' + offset + '&limit=' + limit)
       .map((res) => res.json());
   }
 
   searchGifs(offset, limit, text): Observable<Response> {
     return this.http.get(this.giphyAPIBase + '/search?api_key=' +
       this.APIKEY + '&offset=' + offset +
-      '&limit=' + limit + '&q=' + text + '/')
+      '&limit=' + limit + '&q=' + text)
       .map((res) => res.json());
   }
 }
